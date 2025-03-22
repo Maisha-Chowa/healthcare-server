2915d790b0391041fd351b8e9d3c4ba099f3b8d3
Maisha Maliha Chowa <85276396+Maisha-Chowa@users.noreply.github.com>
2025-03-22 23:21:45 +0600

    doctor-schedules

---
+++ prisma/migrations/20250322171829_update_doctor_schedule/migration.sql
@@ -0,0 +1,9 @@
+/*
+  Warnings:
+
+  - Made the column `isBooked` on table `doctor_schedules` required. This step will fail if there are existing NULL values in that column.
+
+*/
+-- AlterTable
+ALTER TABLE "doctor_schedules" ALTER COLUMN "isBooked" SET NOT NULL,
+ALTER COLUMN "isBooked" SET DEFAULT false;
--- src/app/modules/DoctorSchedule/doctorSchedule.routes.ts
+++ src/app/modules/DoctorSchedule/doctorSchedule.routes.ts
@@ -1,9 +1,9 @@
 import express from "express";
-// import { DoctorScheduleController } from "./doctorSchedule.controller";
-// import auth from "../../middlewares/auth";
-// import { UserRole } from "@prisma/client";
-// import validateRequest from "../../middlewares/validateRequest";
-// import { DoctorScheduleValidation } from "./doctorSchedule.validation";
+import { DoctorScheduleController } from "./doctorSchedule.controller";
+import auth from "../../middlewares/auth";
+import { UserRole } from "@prisma/client";
+import validateRequest from "../../middlewares/validateRequest";
+import { DoctorScheduleValidation } from "./doctorSchedule.validation";
 
 const router = express.Router();
 
@@ -12,29 +12,29 @@ const router = express.Router();
 //  *
 //  * Get all doctor schedule with filtering
 //  */
-// router.get(
-//   "/",
-//   auth(UserRole.SUPER_ADMIN, UserRole.ADMIN, UserRole.DOCTOR, UserRole.PATIENT),
-//   DoctorScheduleController.getAllFromDB
-// );
+router.get(
+  "/",
+  auth(UserRole.SUPER_ADMIN, UserRole.ADMIN, UserRole.DOCTOR, UserRole.PATIENT),
+  DoctorScheduleController.getAllFromDB
+);
 
-// router.get(
-//   "/my-schedule",
-//   auth(UserRole.DOCTOR),
-//   DoctorScheduleController.getMySchedule
-// );
+router.get(
+  "/my-schedule",
+  auth(UserRole.DOCTOR),
+  DoctorScheduleController.getMySchedule
+);
 
-// router.post(
-//   "/",
-//   auth(UserRole.DOCTOR),
-//   validateRequest(DoctorScheduleValidation.create),
-//   DoctorScheduleController.insertIntoDB
-// );
+router.post(
+  "/",
+  auth(UserRole.DOCTOR),
+  validateRequest(DoctorScheduleValidation.create),
+  DoctorScheduleController.insertIntoDB
+);
 
-// router.delete(
-//   "/:id",
-//   auth(UserRole.DOCTOR),
-//   DoctorScheduleController.deleteFromDB
-// );
+router.delete(
+  "/:id",
+  auth(UserRole.DOCTOR),
+  DoctorScheduleController.deleteFromDB
+);
 
 export const DoctorScheduleRoutes = router;
