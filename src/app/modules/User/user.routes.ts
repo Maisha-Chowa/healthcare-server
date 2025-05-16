02535a2e5f40c7145b1a33ebc59ab7edcd34eda5
Maisha Maliha Chowa <85276396+Maisha-Chowa@users.noreply.github.com>
2025-05-16 12:54:21 +0600

    seeding-super-admin

--- src/app/modules/User/user.routes.ts
+++ src/app/modules/User/user.routes.ts
@@ -73,7 +73,7 @@ router.get(
 );
 router.post(
   "/create-admin",
-  auth(UserRole.ADMIN, UserRole.SUPER_ADMIN),
+  // auth(UserRole.ADMIN, UserRole.SUPER_ADMIN),
   fileUploader.upload.single("file"),
   (req: Request, res: Response, next: NextFunction) => {
     req.body = userValidation.createAdmin.parse(JSON.parse(req.body.data));
